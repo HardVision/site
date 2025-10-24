58b7f6010c0d80247343822723e7287103ace39c
JoaoVtC <joao.varaujo@sptech.school>
2025-10-23 23:10:12 -0300

    read do crud feito

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,23 @@
   let listaEmpresasCadastradas = [];
-  
+
+  function showEmpresas(){
+    listaEmpresasCadastradas.forEach((empresa) => {
+        console.log(empresa)
+        tableBody.innerHTML += `<tr>
+                            <td>${empresa.idEmpresa}</td>
+                            <td>${empresa.razaoSocial}</td>
+                            <td>${empresa.nomeFantasia}</td>
+                            <td>${empresa.cnpj}</td>
+                            <td>${empresa.email}</td>
+                            <td>${empresa.telefone}</td>
+                            <td>CEP: ${empresa.cep}<br>${empresa.rua}, ${empresa.numero}</td>
+                            <td>${empresa.token}</td>
+                            <td><img src="assets/atualizar.png" alt=""></td>
+                            <td><img src="assets/lixeira.png" alt=""></td>
+                        </tr>`
+    });
+}
+
   function listar() {
     fetch("/empresas/listar", {
       method: "GET",
@@ -8,14 +26,14 @@
         resposta.json().then((empresas) => {
           empresas.forEach((empresa) => {
             listaEmpresasCadastradas.push(empresa);
-
-            console.log("listaEmpresasCadastradas")
-            console.log(listaEmpresasCadastradas[0].codigo_ativacao)
           });
+        }).then(function (mostrar) {
+            showEmpresas()
         });
       })
       .catch(function (resposta) {
         console.log(`#ERRO: ${resposta}`);
       });
   }
+
 
