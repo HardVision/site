d043b13a3f7efd325aaeef4b2c551e04d9380b72
JoaoVtC <joao.varaujo@sptech.school>
2025-10-29 22:31:35 -0300

    Ajustes nas telas institucionais

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -21,6 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
+
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
