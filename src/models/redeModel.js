7d41ee87ddfcc59756bd8663e29d893e306ad4e4
FelipeLima24 <felipe.qlima@sptech.school>
2025-11-22 23:27:23 -0300

    atualizacaoBackEndRede

--- src/models/redeModel.js
+++ src/models/redeModel.js
@@ -1,15 +1,12 @@
 var dashboardModel = require("../models/dashboardModel");
 
-// NÃO TEM MAIS SQL AQUI.
-// Só reaproveita o que já existe no dashboardModel.
-
 function tempoRealRede(idMaquina) {
-    // usa a mesma query de tempo real usada pela dashboard geral
+    console.log("ACESSEI O REDE MODEL \n\n function tempoRealRede():", idMaquina);
     return dashboardModel.buscarTempoReal(idMaquina);
 }
 
 function historicoRede(idMaquina) {
-    // usa o histórico unificado de rede
+    console.log("ACESSEI O REDE MODEL \n\n function historicoRede():", idMaquina);
     return dashboardModel.historicoRede(idMaquina);
 }
 
